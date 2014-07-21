13296ce1a988093beeb6990a6ed96793cf400520
ohchang <ohchang@try-cat.ch>
2014-07-21 14:08:37 +0200

    태스크E

---
+++ app/controllers/store_controller.rb
@@ -0,0 +1,13 @@
+class StoreController < ApplicationController
+  def index
+  	@products = Product.order(:title)
+
+  	if session[:counter].nil?
+  		session[:counter] = 0
+  	end
+
+  	session[:counter] = session[:counter] + 1
+  	@counter = session[:counter]
+
+  end
+end
